424e20f885479753395b5d9c6fd597a200c31714
Margot Sylvain <margot.sylvain@gmail.com>
2016-11-23 12:37:40 +0100

    commit

--- Gemfile
+++ Gemfile
@@ -4,6 +4,10 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+group :development, :test do
+gem "better_errors"
+gem "binding_of_caller"
+end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+Restaurant.destroy_all
+restaurants_attributes = [
+{
+name: "Epicure au Bristol",
+address: "112 rue du Fg St-Honoré 75008 Paris",
+category: "french",
+phone: "0320377261"
+},
+{
+name: "La truffière",
+address: "4 rue Blainville 75005 Paris",
+category: "italian",
+phone: "4154941079"
+},
+{
+name: "Le pré catelan",
+address: "route de Suresnes 75016 Paris",
+category: "basque",
+phone: "0555562389"
+}
+]
+restaurants_attributes.each { |params| Restaurant.create!(params)}
